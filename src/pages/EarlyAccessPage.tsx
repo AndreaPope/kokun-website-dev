bd02da8a184584a6cbab8348a6689f1e19722467
Andrea Pope <andrea.pope@leaflogistics.com>
2025-05-29 13:27:08 -0400

    another option for early access

--- src/pages/EarlyAccessPage.tsx
+++ src/pages/EarlyAccessPage.tsx
@@ -52,7 +52,7 @@ export default function EarlyAccessPage() {
           //backgroundImage: 'url("https://res.cloudinary.com/dknulbme8/image/upload/v1743104329/cxsfa0mlahnbdvfhln4x.jpg")',
           //height: '100vh', // full screen height
           //backgroundImage: 'url("/images/bridgewmtn.jpg")',
-          backgroundImage: 'url("/images/interestingbridge.jpg")',
+          backgroundImage: 'url("/images/croppedbridge.jpg")',
           backgroundSize: 'cover',
           //backgroundSize: 'contain',
           backgroundRepeat: 'no-repeat',
