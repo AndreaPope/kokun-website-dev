9c63fe6b14e6217fe4b21052f09861b82924cce4
Andrea Pope <andrea.pope@leaflogistics.com>
2025-06-05 12:59:00 -0400

    update button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -33,8 +33,8 @@ export default function Button({
     default: "w-52 md:w-56 h-12 rounded-full text-base font-semibold hover:bg-hover bg-primary text-white",
     small: "px-4 py-2 rounded-full text-xs font-bold hover:bg-hover bg-primary text-white hidden md:inline-flex",
     xsmall: "px-3 py-1 rounded-full text-[10px] font-bold hover:bg-hover bg-primary text-white md:hidden",
-    white: "px-4 py-2 rounded-full text-xs  font-bold hover:bg-hover bg-white text-black hidden md:inline-flex",
-    xsmallwhite: "px-3 py-1 rounded-full text-[10px] font-bold hover:bg-hover bg-white text-black md:hidden",
+    white: "px-4 py-2 rounded-full text-xs  font-bold hover:bg-hover hover:text-white bg-white text-black hidden md:inline-flex",
+    xsmallwhite: "px-3 py-1 rounded-full text-[10px] font-bold hover:bg-hover hover:text-white bg-white text-black md:hidden",
   };
 
   return (
