87b39f707c862e02fd650c587c5ade401e3638bc
Andrea Pope <andrea.pope@leaflogistics.com>
2025-12-18 18:29:32 -0500

    update to team page and local images

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -19,14 +19,12 @@ function AboutPage() {
   const founders: Person[] = [
     {
       name: 'SANGITA JAYARAMAN',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQGzMJx6fmDZDQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1718226509703?e=1765411200&v=beta&t=hSv8fykizIK3NPmvU0AM2W1t7LDRQIZ5AAis-JeNX4w',
+      image:'/images/people/sangita.jpeg',
       linkedin: 'https://www.linkedin.com/in/sangitajayaraman/',
     },
     {
       name: 'ANDREA POPE',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4E03AQH6xxgXf__GMg/profile-displayphoto-scale_200_200/B4EZiEpw3VGUAY-/0/1754572198462?e=1765411200&v=beta&t=rK2SEofvjeuA6qm_cq26NsDPMKLrvdyt3TahfQHVTec',
+      image:'/images/people/andrea.jpeg',
       linkedin: 'https://www.linkedin.com/in/andreapope/',
     },
   ];
@@ -34,13 +32,12 @@ function AboutPage() {
   const advisoryBoard: Person[] = [
     {
       name: 'SEAN NOLAN',
-      image: '/images/SNolan.jpg',
+      image: '/images/people/SNolan.jpg',
       linkedin: 'https://www.linkedin.com/in/familyhealthguy/',
     },
     {
       name: 'DIVYA YERRAGUNTLA',
-      image:
-        'https://media.licdn.com/dms/image/v2/C4D03AQHXoHThZH-O_g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1516251526310?e=1765411200&v=beta&t=tUv3DKqxkdWeaVQYksERdniWzrPC5U6WvMTBsT2tlc0',
+      image:'/images/people/Divya.jfif',
       linkedin: 'https://www.linkedin.com/in/divyayerraguntla/',
     },
   ];
@@ -48,44 +45,37 @@ function AboutPage() {
   const councilMembers: Person[] = [
     {
       name: 'ALIX ALDERMAN',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQFdEQHCG4Xq4A/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1696965251461?e=1765411200&v=beta&t=E_Lz3JXX8MYwbWdZ282cPwKkSzC4iIi24VuRVUTh2qI',
+      image:'/images/people/alix.jpeg',
       linkedin: 'https://www.linkedin.com/in/alixalderman/',
     },
     {
       name: 'ANNA BAUMAN',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQG6R3-ZhfQ2og/profile-displayphoto-shrink_200_200/B56ZPcBCmpGsAY-/0/1734563081601?e=1765411200&v=beta&t=iHMP9TFgcb9f_dZsZjJaSBbhGXH5S4KnIhxRXUT_AcI',
+      image:'/images/people/anna.jpeg',
       linkedin: 'https://www.linkedin.com/in/annabauman/',
     },
     {
       name: 'RAMA DURAIRAJAN',
-      image:
-        'https://media.licdn.com/dms/image/v2/C5603AQE-uoAWg87qLQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1569565028912?e=1765411200&v=beta&t=EgCuE7q6r7TWbXtYhTrbZySYFhRhgEYKEXdbyppx9O0',
+      image:'/images/people/rama.jpeg',
       linkedin: 'https://www.linkedin.com/in/rama-durairajan-2a50ba4/',
     },
     {
       name: 'CAROLYN MOORE',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQEi4Pb1gLkOKg/profile-displayphoto-shrink_200_200/B56ZRNDHy0GQAk-/0/1736459452697?e=1765411200&v=beta&t=F8TBLhQqjZH4pqPCCoHswR4h4LpjarWxHGnK0wkPRZE',
+      image:'/images/people/carolyn.jpeg',
       linkedin: 'https://www.linkedin.com/in/carolynfmoore/',
     },
     {
       name: 'JULIA POLK',
-      image:
-        'https://media.licdn.com/dms/image/v2/C5603AQEywX1ORtjFmg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1652978188951?e=1765411200&v=beta&t=aM2cfpj8KcYJ3SptQD1HlwqTwLnAIS0K_UGXlfPNLsM',
+      image:'/images/people/julia.jpeg',
       linkedin: 'https://www.linkedin.com/in/juliapolk/',
     },
     {
       name: 'BRETT SPITALE',
-      image:
-        'https://media.licdn.com/dms/image/v2/C5603AQF5UfSE71eVfA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1602812013223?e=1765411200&v=beta&t=3zjqSLmCB4PxMKH8TY43rq94hyEozO0m-TzbcLMerJ8',
+      image:'/images/people/brett.jpeg',
       linkedin: 'https://www.linkedin.com/in/brett-spitale-81166b6/',
     },
     {
       name: 'MARK STAHL',
-      image:
-        'https://media.licdn.com/dms/image/v2/C4E03AQGrrclmTLlMyw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517433983331?e=1765411200&v=beta&t=sjs9LBPEOeuzYkvpqtbVhupqOWrNf3XJSdA3qPJj1Bc',
+      image:'/images/people/mark.jpeg',
       linkedin: 'https://www.linkedin.com/in/mark-stahl-81275390/',
     },
   ];
@@ -93,64 +83,71 @@ function AboutPage() {
   const expertContributors: Person[] = [
     {
       name: 'FIONA AKHTAR',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQHM05weBqneAg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1689567226171?e=1765411200&v=beta&t=WEBeK8WSuwQfLCfGaExFqI9SVrJ6elWWc5Y4UOT7v20',
+      image:'/images/people/fiona.jpeg',
       linkedin: 'https://www.linkedin.com/in/fionaa/',
     },
     {
       name: 'THARIKA CHANDRASEKHAR',
-      image: '/images/Tharika.jpg',
+      image: '/images/people/Tharika.jpg',
       linkedin: 'https://www.linkedin.com/in/tharika-chandrasekhar/',
     },
     {
       name: 'RACHEL HEUSSENSTAMM',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQGoo-ZWOIH2lg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1681269369872?e=1765411200&v=beta&t=dSSQcj1I1bK0fUP81ly7fB4VqU7Dtw4swQepJv0jE0g',
+      image:'/images/people/rachel.jpeg',
       linkedin: 'https://www.linkedin.com/in/rachel-heussenstamm/',
     },
     {
       name: 'KAY HOFMEESTER',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQH-QVjoSzIVIg/profile-displayphoto-scale_200_200/B56ZfCqu_YHQAc-/0/1751317674220?e=1765411200&v=beta&t=Qf9Cj8my3P0Laxahc2GL2Fz86WZbU9_3OZl8vsGD9Zg',
+      image:'/images/people/kay.jpeg',
       linkedin: 'https://www.linkedin.com/in/kayhofmeester/',
     },
     {
       name: 'ERIC LEBLANC',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4E03AQFEdyRcZitIZg/profile-displayphoto-scale_200_200/B4EZfB0KtKHYAY-/0/1751303368009?e=1765411200&v=beta&t=57yZ33SmFCojM3XPYZEedWtauF-llyt9L7xJlAM9JlU',
+      image:'/images/people/Eric.jfif',
       linkedin: 'https://www.linkedin.com/in/eric--leblanc/',
     },
   ];
 
   const volunteerMembers: Person[] = [
-    // {
-    //   name: 'MOMINA DIN',
-    //   image:
-    //     'https://media.licdn.com/dms/image/v2/D4D35AQHe5cmltq8QiA/profile-framedphoto-shrink_400_400/B4DZZBqzbCHwAg-/0/1744858462282?e=1764738000&v=beta&t=bhB_OJlql07Ek7tm2guhAYOiaXZqcgILeB_tPtU-Sek',
-    //   linkedin: 'https://www.linkedin.com/in/momina-din/',
-    // },
+    {
+      name: 'EDMOND ABRAHAM',
+      image:'/images/people/edmond.jpg',
+      linkedin: 'https://www.linkedin.com/in/edmondabraham/',
+    },
+    {
+      name: 'KANIKA BANSAL',
+      image:'/images/people/Kanika.jpeg',
+      linkedin: 'https://www.linkedin.com/in/kanikabansal/',
+    },
+    {
+      name: 'DISHITA BAJAJ',
+      image:'/images/people/Dishita.jfif',
+      linkedin: 'https://www.linkedin.com/in/dishita-bajaj-00bb88313/',
+    },
+    {
+      name: 'MOMINA DIN',
+      image:'/images/people/Momina_Din.jpeg',
+      linkedin: 'https://www.linkedin.com/in/momina-din/',
+    },
     {
       name: 'ABHIVYAKTI DUBEY',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4E03AQFOL3rUicPWrA/profile-displayphoto-shrink_200_200/B4EZdWuMYMHYAk-/0/1749506639704?e=1765411200&v=beta&t=52z3C2BGneXnHAlf-PQC8F-eM0ePp5AmKA6KHlPIibg',
+      image:'/images/people/Abhivyakti.jfif',
       linkedin: 'https://www.linkedin.com/in/abhivyaktidubey/',
     },
-    {
-      name: 'SRIVYSHNAVI GADDE',
-      image:
-        'https://media.licdn.com/dms/image/v2/C4E03AQFD1PcVeLhhTQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1517399004412?e=1766016000&v=beta&t=D-iWBbmWLeAirfFznP2vSPwYt8CE7ig2nXXCXesJeB0',
-      linkedin: 'https://www.linkedin.com/in/srivyshnavigadde/',
-    },
+    // {
+    //   name: 'SRIVYSHNAVI GADDE',
+    //   image:'/images/people/Abhivyakti.jfif',
+    //   linkedin: 'https://www.linkedin.com/in/srivyshnavigadde/',
+    // },
     {
       name: 'MIRNA HADDAD',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4D03AQG1rR6QZNrulQ/profile-displayphoto-scale_200_200/B4DZqYiCOsGQAY-/0/1763495651809?e=1765411200&v=beta&t=9qjs6ZAHrx2cDpP6HV3ZSiK09BIthBjRRMv9spWHZrQ',
+      image:'/images/people/Mirna.jfif',
       linkedin: 'https://www.linkedin.com/in/mirna-haddad-67898396/',
     },
+    
     {
       name: 'KEETHANSHAN MARKANDU',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4E03AQGMCXmJr4j2iQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1707718189082?e=1765411200&v=beta&t=DfDGMJpwxmFQEXxC3Zz9SKTRwl74hUzitckWtl4j2Z0',
+      image:'/images/people/Keeth.jfif',
       linkedin: 'https://www.linkedin.com/in/keethanshan-markandu-a79877248/',
     },
     // {
@@ -161,49 +158,45 @@ function AboutPage() {
     // },
     {
       name: 'ROBERT ROJO',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQEhK96HErX81w/profile-displayphoto-scale_400_400/B56ZfsNALYG0Ag-/0/1752014521492?e=1766016000&v=beta&t=PDSIjCbtSL1CDUnMyyMk4nse3kSRlBbqgTE3o3QjdqQ',
+      image:'/images/people/robert.jpeg',
       linkedin: 'https://www.linkedin.com/in/robertrojodev/',
     },
     {
       name: 'SAVANI SHROTRI',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4D03AQEKxMzt7f-rMA/profile-displayphoto-scale_200_200/B4DZlfAfQqJYAY-/0/1758235588245?e=1765411200&v=beta&t=SPBTC2I32pI_8pJZHSBfRPe5AEQ0UK5Dqu4gasCBz5Q',
+      image:'/images/people/Savani.jfif',
       linkedin: 'https://www.linkedin.com/in/savanishrotri/',
     },
-    {
-      name: 'DISHITA BAJAJ',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4E03AQFB2cB6WdguRQ/profile-displayphoto-scale_200_200/B4EZnV4Hb5GoAY-/0/1760229882068?e=1765411200&v=beta&t=x5WsvhanfAs_WdS3bJCtlv8seN2F_igtNuCtE7vbnGI',
-      linkedin: 'https://www.linkedin.com/in/dishita-bajaj-00bb88313/',
-    },
+    
   ];
 
   const volunteerPastMembers: Person[] = [
+    // {
+    //   name: 'NILA KATHIRAVAN',
+    //   image:'/images/people/Nila.jfif',
+    //   linkedin: 'https://www.linkedin.com/in/nila-kathiravan-33b62b359/',
+    // }
     {
-      name: 'NILA KATHIRAVAN',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4D03AQEQnOLhG-rAMA/profile-displayphoto-shrink_200_200/B4DZXgsPnuH4AY-/0/1743231449878?e=1765411200&v=beta&t=QTGe6YrQfMHvlTgW-6rWsmDY-zl91ArLf2AZzQrPtxg',
-      linkedin: 'https://www.linkedin.com/in/nila-kathiravan-33b62b359/',
-    },
+      name: 'PRAYAS DURAIRAJAN',
+      image:'/images/people/prayas.jpeg',
+      linkedin: 'https://www.linkedin.com/in/murari-durairajan-b14409359/',
+    },  
     {
       name: 'JENNY JONES',
-      image:
-        'https://media.licdn.com/dms/image/v2/C5603AQHNYcX7PmIIYA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1654541356674?e=1765411200&v=beta&t=knU61rg-1CXHWGswoOOQQ1NQxLlwO0lRz-ysmpkyqOY',
+      image:'/images/people/Jenny.jfif',
       linkedin: 'https://www.linkedin.com/in/jenny-jones-1740a79/',
     },
     {
       name: 'DHRUV KURPAD',
-      image:
-        'https://media.licdn.com/dms/image/v2/D5603AQFMGkC-hdiO4A/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727240730439?e=1766016000&v=beta&t=rrjfnqDeX1jtVZQKomd-xyszmnhOo942sGULE40KHp8',
+      image:'/images/people/Dhruv.jfif',
       linkedin: 'https://www.linkedin.com/in/dhruv-kurpad-212b60276/',
     },
     {
       name: 'BISHARAT MINHAS',
-      image:
-        'https://media.licdn.com/dms/image/v2/D4D03AQH_d0TO-N-dHQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714658233059?e=1765411200&v=beta&t=MjEoTwsYKSn05ityNNGcrhVPKBcxy--S5q-prvvDk8I',
+      image:'/images/people/Bisharat.jfif',
       linkedin: 'https://www.linkedin.com/in/bisharat-minhas/',
     },
+    
+
   ];
 
   const renderPeopleGrid = (people: Person[]) => (
