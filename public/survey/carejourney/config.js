c22610096415f912d65fee5c2bf82dd414895772
Andrea Pope <andrea.pope@leaflogistics.com>
2026-06-05 10:10:00 -0400

    updates to config.js

--- public/survey/carejourney/config.js
+++ public/survey/carejourney/config.js
@@ -1,4 +1,4 @@
 const SUPABASE_CONFIG = {
-  url: "https://euzvqbrzdzgsbtuuifoi.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1enZxYnJ6ZHpnc2J0dXVpZm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMzUwNDAsImV4cCI6MjA5NDgxMTA0MH0.AV6dKxJCmubZ7JM9xW1C75g0UcD_rf45hIwRpsOQ4F8"
+  url: "https://yjngyftaaenftmksjxbn.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqbmd5ZnRhYWVuZnRta3NqeGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3ODc2MTQsImV4cCI6MjA3ODM2MzYxNH0.o0VJjivwm4ylfZv3tBpHaSC6tg0Rzhw4vBDoRhxmSII"
 };
